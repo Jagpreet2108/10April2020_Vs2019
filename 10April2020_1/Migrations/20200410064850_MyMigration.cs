4dc797ab5be61d54bbee1d838cb1631655da7d3e
Jagpreet2108 <jagpreetkaur2108@gmail.com>
2020-04-10 14:58:22 +0530

    InitialCommit

---
+++ 10April2020_1/Migrations/20200410064850_MyMigration.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace _10April2020_1.Migrations
+{
+    public partial class MyMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Customer",
+                columns: table => new
+                {
+                    CId = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    CName = table.Column<string>(nullable: true),
+                    DOB = table.Column<DateTime>(nullable: false),
+                    CPin = table.Column<int>(maxLength: 4, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Customer", x => x.CId);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Customer",
+                columns: new[] { "CId", "CName", "CPin", "DOB" },
+                values: new object[,]
+                {
+                    { 1, "Sameer", 1122, new DateTime(2020, 4, 10, 0, 0, 0, 0, DateTimeKind.Unspecified) },
+                    { 2, "Zoya", 1111, new DateTime(2020, 4, 11, 0, 0, 0, 0, DateTimeKind.Unspecified) },
+                    { 3, "Chetna", 2222, new DateTime(2020, 4, 12, 0, 0, 0, 0, DateTimeKind.Unspecified) },
+                    { 4, "Kartik", 3322, new DateTime(2020, 5, 12, 0, 0, 0, 0, DateTimeKind.Unspecified) }
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Customer");
+        }
+    }
+}
---
+++ 10April2020_1/Models/CustomerContext.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace _10April2020_1.Models
+{
+    public class CustomerContext : DbContext
+    {
+        public CustomerContext([NotNullAttribute] DbContextOptions options) : base(options)
+        { }
+
+        public DbSet<Customer> Customer { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Customer>().HasData
+                (
+             new Customer { CId = 1, CName = "Sameer", DOB = new DateTime(day: 10, month: 04, year: 2020), CPin = 1122 },
+             new Customer { CId = 2, CName = "Zoya", DOB = new DateTime(day: 11, month: 04, year: 2020), CPin = 1111 },
+             new Customer { CId = 3, CName = "Chetna", DOB = new DateTime(day: 12, month: 04, year: 2020), CPin = 2222 },
+             new Customer { CId = 4, CName = "Kartik", DOB = new DateTime(day: 12, month: 05, year: 2020), CPin = 3322 }
+
+                ); 
+        }
+    }
+}
